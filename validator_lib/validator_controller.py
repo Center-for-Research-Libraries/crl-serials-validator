43fb6ea7d217b65654c671ec483c40d6672e03f7
Nathaniel Florin <nflorin@gmail.com>
2021-09-01 17:50:01 -0400

    Removed separate logging file and moved logger into controller.

--- validator_lib/validator_controller.py
+++ validator_lib/validator_controller.py
@@ -1,13 +1,19 @@
 import logging
+import os
+import datetime
 
 from validator_lib.validator_logger import set_validator_logger
-from validator_lib.utilities import get_input_files, initialize_folders
+from validator_lib.utilities import get_input_files, initialize_folders, get_directory_location
 from crl_lib.api_key_setter import ApiKeySetter
 from crl_lib.issn_db import IssnDb
 from validator_lib.choose_input_file_fields import InputFieldsChooser
 from validator_lib.scan_input_files import InputFileScanner
 from validator_lib.run_checks_process import ChecksRunner
 from validator_lib.choose_disqualifying_issues import IssuesChooser
+
+
+# Setting this to True will turn on debugging messages in the logging and automatically log to the screen
+DEBUG_MODE = False
 
 
 class ValidatorController:
@@ -21,38 +27,69 @@ class ValidatorController:
         self.headless_mode = headless_mode
         self.log_to_screen = log_to_screen
 
+        if DEBUG_MODE is True:
+            self.log_to_screen = True
+
         # Make sure the relevant folders exist.
         initialize_folders()
 
         self.input_files_seen = False
         self.marc_input_seen = False
 
-        set_validator_logger(log_to_screen)
+        self.set_validator_logger()
 
-        logging.info('Beginning validation process.')
+        self.logger.info('Beginning validation process.')
 
         self.check_input_folder()
         self.check_issn_db()
         
         if self.headless_mode is True:
-            logging.info('Running in headless mode.')
+            self.logger.info('Running in headless mode.')
             self.run_headless()
+
+    def set_validator_logger(self):
+        self.logger = logging.getLogger('validator')
+        log_directory = get_directory_location('logs')
+        log_file_name = 'validator_log_{:%Y-%m-%d}.log'.format(datetime.datetime.now())
+        log_file = os.path.join(log_directory, log_file_name)
+
+        fh = logging.FileHandler(log_file)
+        ch = logging.StreamHandler()
+
+        fh_formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+        ch_formatter = logging.Formatter('%(message)s')
+        
+        if self.log_to_screen is True:
+            if DEBUG_MODE is True:
+                fh.setLevel(logging.DEBUG)
+                ch.setLevel(logging.DEBUG)
+            else:
+                fh.setLevel(logging.INFO)
+                ch.setLevel(logging.INFO)
+            ch.setFormatter(ch_formatter)
+            self.logger.addHandler(ch)
+        else:
+            fh.setLevel(logging.INFO)
+            
+        fh.setFormatter(ch_formatter)
+        self.logger.addHandler(fh)
 
     def run_headless(self):
         self.download_api_data()
         self.run_checks_process()
 
     def check_issn_db(self):
-        issn_db = IssnDb()
-        if issn_db.conn is not None:
-            logging.info('ISSN database is installed.')
+        issn_db = IssnDb(ignore_missing_db=True)
+        if issn_db.found_issn_db is True:
+            self.logger.info('ISSN database is installed.')
 
     def check_input_folder(self):
         input_files = get_input_files()
         if not input_files:
-            logging.warning('No input files found.')
+            self.logger.warning('No input files found.')
         for input_file in input_files:
-            logging.info('Saw input file {}'.format(input_file))
+            self.logger.info('Saw input file {}'.format(input_file))
             for input_format in self.viable_input_formats:
                 if input_file.lower().endswith(input_format):
                     self.input_files_seen = True
@@ -64,7 +101,7 @@ class ValidatorController:
         ApiKeySetter()
     
     def scan_input_files(self):
-        InputFileScanner()
+        InputFileScanner(print_to_screen=self.log_to_screen)
 
     def choose_input_fields(self):
         self.print_popunder_window_warning()
