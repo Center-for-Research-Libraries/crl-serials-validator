6c05b04299f6d63d180d0a63cad1ed419e673216
Nathaniel Florin <nflorin@gmail.com>
2021-09-27 09:31:53 -0400

    Created very basic theming library

---
+++ validator_lib/ttk_theme.py
@@ -0,0 +1,36 @@
+from tkinter import ttk
+import tkinter as tk
+
+def set_theme(tk_object=None):
+    """
+    This will be used to set a default ttk style, once we come up with one.
+    """
+
+    bg_color = '#fafafa'
+    fg_color = '#202020'
+    disabled_fg_color = "#a0a0a0"
+    selected_fg_color = "#ffffff"
+    selected_bg_color = "#2f60d8"
+
+    # if tk_object:
+    #     tk_object.configure(bg=bg_color)
+
+    style = ttk.Style()
+    style.theme_use('clam')
+
+    # style.configure('TFrame', background=bg_color, foreground=fg_color)
+    # style.configure('TLabel', background=bg_color, foreground=fg_color)
+    # style.configure('TCheckbutton', background=bg_color, foreground=fg_color, selected=selected_fg_color)
+
+    style.configure('link.TButton', foreground='darkblue')
+
+    style.configure('save.TButton')
+    # style.map('save.TButton', background=[('active', 'lightgreen')])
+
+    style.configure('cancel.TButton')
+    # style.map('cancel.TButton', background=[('active', '#FF7F7F')])
+
+    style.configure('defaults.TButton')
+    # style.map('defaults.TButton', background=[('active', 'lightblue')])
+
+    return style
