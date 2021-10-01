24b97838e2e83bfb9d5cfe23ef5506dba7c84986
Nathaniel Florin <nflorin@gmail.com>
2021-10-01 15:53:08 -0400

    Switch to getting issues from regular of bulk configs.

--- validator_lib/run_mrk_process.py
+++ validator_lib/run_mrk_process.py
@@ -8,7 +8,7 @@ from crl_lib.line_85x86x import Convert85x86x
 from crl_lib.marc_fields import MarcFields
 from crl_lib.marc_file_reader import MarcFileReader
 from crl_lib.crl_utilities import clean_oclc
-from validator_lib.validator_config import ValidatorConfig
+from validator_lib.validator_config import FieldsAndIssuesFinder
 from validator_lib.validate_583s import Line583Validator
 from validator_lib.utilities import get_first_last_year_from_regular_holdings
 from validator_lib.supplements_and_indexes_functions import remove_supplements_from_holdings, remove_indexes_from_holdings
@@ -20,8 +20,8 @@ class MrkProcessRunner:
         self.input_file = input_file
         self.input_file_location = os.path.join(os.getcwd(), 'input', self.input_file)
 
-        validator_config = ValidatorConfig()
-        self.input_fields = validator_config.get_input_fields(self.input_file)
+        fields_and_issues_finder = FieldsAndIssuesFinder()
+        self.input_fields = fields_and_issues_finder.get_fields_for_individual_file(self.input_file)
 
         self.line_583_validator = Line583Validator()
 
