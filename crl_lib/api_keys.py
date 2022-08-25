1aaeb69f8b4e783a3c31802f86a0c927509b6eef
Nate Florin <nflorin@gmail.com>
2022-08-25 13:24:14 -0400

    Update to follow upstream api_keys.py. Switches config file to YAML from a .ini file.

--- crl_lib/api_keys.py
+++ crl_lib/api_keys.py
@@ -1,7 +1,10 @@
 import random
 import os
 import configparser
+import yaml
 import sys
+
+from crl_lib.crl_file_locations import get_api_key_file_location
 
 
 class OclcApiKeys:
@@ -19,112 +22,148 @@ class OclcApiKeys:
 
     """
     api_keys = {}
+    api_key_config_file_location = None
 
-    def __init__(self, api_key_config_file_location, name_for_key=None):
-        
-        if os.path.isdir(api_key_config_file_location):
-            api_key_config_file_location = os.path.join(api_key_config_file_location, 'api_keys.ini')
-        
-        self.name_for_key = name_for_key
+    def __init__(self, name: str = '', api_key_config_file_location: str = '') -> None:
 
-        self.api_key_config_file_location = api_key_config_file_location
+        if api_key_config_file_location and not api_key_config_file_location.endswith('yaml'):
+            api_key_config_file_location = os.path.join(api_key_config_file_location, 'api_keys.yaml')
 
+        self.name_for_key = name
+        self.find_config_file(api_key_config_file_location)
         self.check_api_key_config_file_location()
+        self.api_keys = {}
+
+        self.api_key_name = ''
+        self.config = ''
 
         self.read_config_file()
 
-    def __del__(self):
+    def __del__(self) -> None:
         pass
 
-    def read_config_file(self):
-        self.config = configparser.ConfigParser()
-        self.config.read(self.api_key_config_file_location)
+    def reset_api_keys(self) -> None:
+        """
+        Set the YAML config dict to the default if no data exists
+        """
+        self.api_keys = {}
 
-        self.get_all_api_keys()
-        self.api_key_name = self.set_api_key_name(self.name_for_key)
+    def find_config_file(self, api_key_config_file_location: str) -> None:
+        if not api_key_config_file_location:
+            api_key_config_file_location = get_api_key_file_location()
+        self.api_key_config_file_location = api_key_config_file_location
+
+    def make_config_directory(self, my_dir) -> None:
+        """
+        Make the configuration directory if it doesn't exist.
+        This function will recurse until it either fails or creates the directory.
+        """
+        if os.path.isdir(my_dir):
+            return
+        sub_dir = os.path.split(my_dir)[0]
+        if sub_dir == my_dir:
+            raise Exception("Can't make directory for configuration file.")
+        if not os.path.isdir(sub_dir):
+            self.make_config_directory(sub_dir)
+        os.mkdir(my_dir)
+
+    def check_api_key_config_file_location(self) -> None:
+        if not os.path.isfile(self.api_key_config_file_location):
+            old_file_location = self.api_key_config_file_location.replace('api_keys.yaml', 'api_keys.ini')
+            print(old_file_location)
+            if os.path.isfile(old_file_location):
+                self.replace_old_config_file(old_file_location)
+            config_dir = os.path.split(self.api_key_config_file_location)[0]
+            self.make_config_directory(config_dir)
+            if not os.path.isfile(self.api_key_config_file_location):
+                self.reset_api_keys()
+                self.write_preferences_to_file()
+
+    def read_config_file(self) -> None:
+        with open(self.api_key_config_file_location, 'r', encoding='utf8') as fin:
+            self.api_keys = yaml.full_load(fin)
 
     @property
-    def api_key(self):
+    def api_key(self) -> str:
         try:
-            return self.config["API KEYS"][self.api_key_name]
+            return self.api_keys['KEYS'][self.api_key_name]
         except AttributeError:
-            # no key set, probably because none in config.ini
-            return None
+            # no key set, probably because none in api_keys.yaml
+            return ''
 
     @api_key.setter
-    def api_key(self, name_for_key=None):
+    def api_key(self, name_for_key: str = '') -> None:
         self.set_api_key_name(name_for_key)
 
-    def get_api_key_name(self):
+    def get_api_key_name(self) -> str:
         return self.api_key_name
-
-    def get_all_api_keys(self):
-        try:
-            for name in self.config['API KEYS'].keys():
-                self.api_keys[name] = self.config['API KEYS'][name]
-        except KeyError:
-            # Add section, so that a segment will at least be added to config.ini
-            self.config.add_section('API KEYS')
-
-    def check_api_key_config_file_location(self):
-        if not os.path.isfile(self.api_key_config_file_location):
-            try:
-                fout = open(self.api_key_config_file_location, "w", encoding="utf8")
-                fout.close()
-            except FileNotFoundError:
-                print('API config file could not be created. Non-existent folder location?')
-                sys.exit('Location is {}'.format(self.api_key_config_file_location))
-            except PermissionError:
-                print('API config file could not be created due to PermissionError.')
-                sys.exit('Location is {}'.format(self.api_key_config_file_location))
                 
-    def set_preferred_api_key_name(self, name):
+    def set_preferred_api_key_name(self, name: str) -> None:
         """
         Set the preferred API key name in the preferences. To make this permanent,
         the preferences file should be re-written at some point after this.
         """
         name = name.lower()
         try:
-            self.config['API KEYS'][name]
+            self.api_keys[name]['DEFAULT'] = '1'
         except KeyError:
             raise KeyError(
-                "Can't use name {} as preferred API key; not in list of keys.".format(name))
-        self.config['Preferred API Key'] = {name: '1'}
+                f"Can't use name {name} as preferred API key; not in list of keys.")
 
-    def set_api_key_name(self, name_for_key=None):
-        # No API keys read. THe user will have to add some, or the base script will have to deal with the issue
-        if not self.config["API KEYS"]:
-            self.api_key_name = None
-            return
-
+    def set_api_key_name(self, name_for_key: str = '') -> None:
+        # No API keys read. The user will have to add some, or the base script will have to deal with the issue
+        if not self.api_keys:
+            raise KeyError(f"No API keys set?")
+        self.api_key_name = ''
         if name_for_key:
-            if name_for_key not in self.config["API KEYS"]:
-                raise KeyError(
-                    "Name {} not in API key list.".format(name_for_key))
+            if name_for_key not in self.api_keys:
+                raise KeyError(f"Name {name_for_key} not in API key list.")
             self.api_key_name = name_for_key
-            return
-        if "Preferred API Key" in self.config:
-            preferred_name = None
-            for name in self.config["Preferred API Key"]:
-                if not preferred_name:
-                    preferred_name = name
-                else:
-                    # Remove extra preferred names from the preferences.
-                    # This is ultimately only meaningful in the prefs are then
-                    # written back to the file.
-                    self.remove_preference_key("Preferred API Key", name)
-                if preferred_name:
-                    return preferred_name
-        # Fallback, choose a random name if none preferred.
-        name = random.choice(list(self.api_keys.keys()))
-        return name
+        else:
+            for name in self.api_keys:
+                if self.api_keys[name]['DEFAULT']:
+                    if not self.api_key_name:
+                        self.api_key_name = name
+                    else:
+                        self.api_keys[name]['DEFAULT'] = ''
+        if not self.api_key_name:
+            # Fallback, choose a random name if none preferred.
+            self.api_key_name = random.choice(list(self.api_keys.keys()))
 
-    def remove_preference_key(self, section, key):
+    def remove_name(self, name: str) -> None:
         try:
-            self.config[section].pop(key)
+            self.api_keys.pop(name)
         except KeyError:
             pass
 
-    def write_preferences_to_file(self):
-        with open(self.api_key_config_file_location, "w", encoding="utf8") as fout:
-            self.config.write(fout)
+    def write_preferences_to_file(self) -> None:
+        try:
+            with open(self.api_key_config_file_location, "w", encoding="utf8") as fout:
+                yaml.dump(self.api_keys, fout)
+        except PermissionError:
+            print('API config file could not be created due to PermissionError.')
+            sys.exit(f'Location is {self.api_key_config_file_location}')
+
+    def replace_old_config_file(self, old_file_location: str) -> None:
+        """
+        Read any old api_keys.ini files found and replace them with api_keys.yaml file.
+        """
+        yaml_dict = {}
+        config = configparser.ConfigParser()
+        config.read(old_file_location)
+        print(old_file_location)
+        for name in config['API KEYS']:
+            print(name)
+            yaml_dict[name] = {
+                'KEY': config['API KEYS'][name],
+                'SECRET': '',
+                'DEFAULT': '',
+            }
+        try:
+            for name in config['Preferred API Key']:
+                yaml_dict[name]['DEFAULT'] = '1'
+        except KeyError:
+            pass
+        print(yaml_dict)
+        with open(self.api_key_config_file_location, 'w', encoding='utf8') as fout:
+            yaml.dump(yaml_dict, fout)
