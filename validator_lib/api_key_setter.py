f69cab38de1f6b9957a80677fef1f40473de029f
Nate Florin <nflorin@gmail.com>
2022-08-25 16:13:07 -0400

    Updates to work with new API key formats

--- validator_lib/api_key_setter.py
+++ validator_lib/api_key_setter.py
@@ -1,12 +1,17 @@
 import os
 import sys
-from termcolor import colored
+from termcolor import colored, cprint
 from collections import OrderedDict
 
-from validator_lib.terminal_gui_utilities import print_terminal_page_header
-from validator_lib.validator_data import CRL_FOLDER
-
 from crl_lib.api_keys import OclcApiKeys
+
+
+def print_terminal_page_header(header_str, header_color='green'):
+    header_bar = ''.join(['~' for _ in header_str])
+    cprint(header_bar, header_color)
+    cprint(header_str, header_color)
+    cprint(header_bar, header_color)
+    print('')
 
 
 class ApiKeySetter:
@@ -14,49 +19,56 @@ class ApiKeySetter:
     Class to read and write API keys from/to the config file. This is mainly a
     GUI front end for the OclcApiKeys library.
     """
+
     def __init__(self):
+        self.api_keys = None
+        self.names = []
         self.make_gui()
 
     @staticmethod
     def print_row_to_terminal(
-        number_column, name, api_key, is_default_print, header_row=False):
+            number_column, name, api_key, api_secret, is_default_print, header_row=False):
         print('{}\t{}\t{}\t{}'.format(
-            colored(str(number_column), 
-            'yellow').ljust(4), 
-            name.ljust(12), 
-            api_key.ljust(80), 
+            colored(str(number_column),
+                    'yellow').ljust(4),
+            name.ljust(12),
+            api_key.ljust(80),
+            api_secret.ljust(20),
             is_default_print))
         if header_row is True:
-            for _ in range(0, 120):
+            for _ in range(0, 150):
                 print('-', end='')
             print('')
 
     def make_gui(self):
-        self.api_keys = OclcApiKeys(CRL_FOLDER)
 
         while True:
+            self.api_keys = OclcApiKeys()
+
             os.system('cls' if os.name == 'nt' else 'clear')
 
             print_terminal_page_header('Set API Keys')
 
-            self.get_names()
-            self.print_row_to_terminal(
-                ' ', 'Name', 'API Key', '', header_row=True)
+            self.names = list(self.api_keys.api_keys.keys())
+            self.names.insert(0, '')
 
-            i = 0
-            for name in self.names_and_keys:
+            self.print_row_to_terminal(
+                ' ', 'Name', 'API Key', 'API Secret', '', header_row=True)
+
+            for i in range(1, len(names)):
+                name = self.names[i]
                 if not name:
                     continue
-                i += 1
                 is_default_print = ''
-                if name == self.default_key:
+                api_key = self.api_keys.api_keys[name]['KEY']
+                api_secret = self.api_keys.api_keys[name]['SECRET']
+                if self.api_keys.api_keys[name]['DEFAULT'] == '1':
                     is_default_print = colored('default', 'white', 'on_blue')
-                self.print_row_to_terminal(
-                    i, name, self.names_and_keys[name]['api_key'],
-                    is_default_print)
+                self.print_row_to_terminal(i, name, api_key, api_secret, is_default_print)
 
             print('')
             print('{}. Add a new key.'.format(colored('a', 'yellow')))
+            print('{}. Edit a key.'.format(colored('e', 'yellow')))
             print('{}. Remove a key.'.format(colored('r', 'yellow')))
             print('{}. Set a new default key.'.format(colored('d', 'yellow')))
             print('{}. Back to the main menu.'.format(colored('m', 'yellow')))
@@ -65,41 +77,14 @@ class ApiKeySetter:
             choice_result = input('Your choice? ')
 
             if choice_result.lower() == 'a':
-                new_name = input('{} name for key: '.format(
-                    colored('Enter', 'yellow')))
-                new_key = input('{} API key: '.format(
-                    colored('Enter', 'yellow')))
-                new_is_default = input(
-                    'Make default key? ({}/{}) '.format(
-                        colored('y', 'yellow'), colored('n', 'yellow')))
-                if not new_name:
-                    print('No name given; key not added.')
-                elif not new_key:
-                    print('No key input; nothing added.')
-                else:
-                    if new_is_default.lower().startswith('y'):
-                        make_default = True
-                    else:
-                        make_default = False
-                    self.add_key(new_name, new_key, make_default)
+                self.add_key()
+
             elif choice_result.lower() == 'r':
-                to_delete = input('{} number to delete: '.format(
-                    colored('Enter', 'yellow')))
-                try:
-                    int(to_delete)
-                    self.delete_key(to_delete)
-                except (ValueError, TypeError):
-                    print('Must enter a number to delete.')
-                    input('Press Enter to continue.')
+                self.delete_key()
+
             elif choice_result.lower() == 'd':
-                new_default = input('{} number to set as default: '.format(
-                    colored('Enter', 'yellow')))
-                try:
-                    int(new_default)
-                    self.set_default_key(int(new_default))
-                except (ValueError, TypeError):
-                    print('Must enter a number to set default key.')
-                    input('Press Enter to continue.')
+                self.set_default_key()
+
             elif choice_result.lower() == 'm':
                 break
             elif choice_result.lower() == 'q':
@@ -108,49 +93,52 @@ class ApiKeySetter:
                 print("I didn't understand that.")
             print('')
 
-    def add_key(self, new_name, new_key, make_default):
-        self.names_and_keys[new_name] = {'api_key': new_key}
-        if make_default is True:
-            self.default_key = new_name
-        self.save_api_keys()
+    def add_key(self):
+        new_name = input('{} name for key: '.format(colored('Enter', 'yellow')))
+        new_key = input('{} API key: '.format(colored('Enter', 'yellow')))
+        new_secret = input('{} API secret (if applicable): '.format(colored('Enter', 'yellow')))
+        new_is_default = input('Make default key? ({}/{}) '.format(colored('y', 'yellow'), colored('n', 'yellow')))
+        if not new_name:
+            print('No name given; key not added.')
+        elif not new_key:
+            print('No key input; nothing added.')
+        else:
+            if new_is_default.lower().startswith('y'):
+                make_default = '1'
+            else:
+                make_default = ''
 
-    def delete_key(self, number_to_delete):
-        for name in self.names_and_keys:
-            if int(number_to_delete) == self.names_and_keys[name]['number']:
-                self.names_and_keys.pop(name)
-                if self.default_key == 'name':
-                    self.default_key = None
-        self.save_api_keys()
+            self.api_keys.api_keys[new_name] = {
+                'KEY': new_key,
+                'SECRET': new_secret,
+                'DEFAULT': make_default
+            }
+            self.api_keys.write_preferences_to_file()
 
-    def set_default_key(self, number_as_default):
-        for name in self.names_and_keys:
-            if self.names_and_keys[name]['number'] == number_as_default:
-                self.default_key = name
-        self.save_api_keys()
+    def delete_key(self):
+        to_delete = input('{} number to delete: '.format(colored('Enter', 'yellow')))
+        try:
+            name_to_delete = self.names[int(to_delete.strip())]
+            try:
+                self.api_keys.api_keys.pop(name_to_delete)
+                self.api_keys.write_preferences_to_file()
+            except IndexError:
+                pass
+        except (ValueError, TypeError, IndexError):
+            print('Must enter a valid number to delete.')
+            input('Press Enter to continue.')
 
-    def get_names(self):
-        self.api_keys.read_config_file()
-        self.names_and_keys = OrderedDict()
-        self.default_key = None
-        self.api_keys.get_all_api_keys()
-        number = 0
-        for name in self.api_keys.config['API KEYS']:
-            number += 1
-            api_key = self.api_keys.api_keys[name]
-            if self.api_keys.api_key_name == name:
-                self.default_key = name
-            self.names_and_keys[name] = {'api_key': api_key, 'number': number}
-
-    def save_api_keys(self):
-        """Look for API keys and default key set and save to config file."""
-        self.api_keys.config['API KEYS'] = {}
-        self.api_keys.config['Preferred API Key'] = {}
-        for name in self.names_and_keys:
-            if name and self.names_and_keys[name]['api_key']:
-                self.api_keys.config['API KEYS'][name] = self.names_and_keys[name]['api_key']
-                if name == self.default_key:
-                    self.api_keys.config['Preferred API Key'] = {name: 1}
-        self.api_keys.write_preferences_to_file()
+    def set_default_key(self):
+        new_default = input('{} number to set as default: '.format(colored('Enter', 'yellow')))
+        try:
+            default_name = self.names[int(new_default)]
+            for name in self.api_keys.api_keys:
+                self.api_keys.api_keys[name]['DEFAULT'] = ''
+            self.api_keys.api_keys[default_name]['DEFAULT'] = '1'
+            self.api_keys.write_preferences_to_file()
+        except (ValueError, TypeError, IndexError):
+            print('Must enter a number to set default key.')
+            input('Press Enter to continue.')
 
 
 if __name__ == "__main__":
